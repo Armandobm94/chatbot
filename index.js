8e21bdc976599c5d3135e5db84784c44184b3e53
Armando Begazo Mogrovejo <armando.begazo94@hotmail.com>
2017-09-21 01:52:42 -0500

    Habilitando funcionalidad del bot 5

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ app.post('/webhook/',function(req,res){
             let text = event.message.text
             if(text.includes("Hola")){
             sendText(sender, "Como estas?")
-            }else if(text.includes("Como estas?")){
-            sendText(sender, "Aun soy un bot de prueba :(")
+            }else if(text.includes("bien")){
+            sendText(sender, "Yo aun soy un bot de prueba :(")
             }else{
             sendText(sender, "Me dijiste: " + text.substring(0,100))
             }
